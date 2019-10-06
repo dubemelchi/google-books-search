65d2ec5b30775c4452ebb46d12784d50f74d1bdf
dubemelchi <dubemelchi@gmail.com>
2019-10-06 12:45:56 -0400

    display results

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,12 @@ const App = () => {
   let API_URL = `https://www.googleapis.com/books/v1/volumes?q=<searchTerm>
 `;
 
+  const [books, setBooks] = useState({ items: [] });
+
   const fetchBooks = async () => {
     // ajax api using axios
     const result = await axios.get(`${API_URL}?q=${searchTerm}`);
+    setBooks(result.data);
     // results
     console.log(result.data);
   };
@@ -39,6 +42,25 @@ const App = () => {
           <button type='submt'>Search</button>
         </label>
       </form>
+      <ul>
+        {books.items.map((book, index) => {
+          return (
+            <li key={index}>
+              <div>
+                <img
+                  alt={`${book.volumeInfo.title} book`}
+                  src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
+                />
+                <div>
+                  <h3>{book.volumeInfo.tite}</h3>
+                  <p>{book.volumeInfo.publishedDate}</p>
+                </div>
+              </div>
+              <hr />
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
 };
