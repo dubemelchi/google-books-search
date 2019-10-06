57e7ece48574b4762dcf11a23949ee461a2ceac8
dubemelchi <dubemelchi@gmail.com>
2019-10-06 12:10:12 -0400

    useState hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const onInputChange = e => {
+    setSearchTerm(e.target.value);
+  };
   return (
     <section>
       <form>
         <label>
           <span>Search for books</span>
-          <input type='search' placeholder='something something' />
+          <input
+            type='search'
+            placeholder='something something'
+            value={searchTerm}
+            onChange={onInputChange}
+          />
           <button type='submt'>Search</button>
         </label>
       </form>
